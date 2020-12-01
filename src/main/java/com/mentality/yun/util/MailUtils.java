84f04a0e16c7b4db952d641267c0addb3e0b00c0
Steven-Yang99 <17608520784@163.com>
2020-12-01 17:23:33 +0800

    抹掉邮箱即邮箱授权码（安全起见）

--- src/main/java/com/mentality/yun/util/MailUtils.java
+++ src/main/java/com/mentality/yun/util/MailUtils.java
@@ -9,8 +9,8 @@ import java.util.Properties;
  * 发邮件工具类
  */
 public final class MailUtils {
-    private static final String USER = "676840009@qq.com"; // 发件人称号，同邮箱地址
-    private static final String PASSWORD = "wkbuxppirssrbbhh"; // 如果是qq邮箱可以使户端授权码，或者登录密码
+    private static final String USER = "xxxxxxx"; // 发件人称号，同邮箱地址
+    private static final String PASSWORD = "xxxxx"; // 如果是qq邮箱可以使户端授权码，或者登录密码
 
     /**
      * @param to    收件人邮箱
